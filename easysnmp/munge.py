7a717e04e22fc37b8ba620a0d9980e757ec0e183
Gabriele Mambrini <g.mambrini@gmail.com>
2012-05-13 22:46:31 +0200

    separated oid mungers from value mungers

--- easysnmp/munge.py
+++ easysnmp/munge.py
@@ -2,13 +2,17 @@
 def hex2dec(i):
     return int(i, 16)
 
-def munge_macaddress(octects):
-    """This Function will convert a MacAddress octet string"""
+def munge_macaddress_ids(octects):
+    """This Function will convert an id sequence to a mac address"""
     return ":".join(map(lambda i: "%02x" % i, octects))
 
+def munge_macaddress(octects):
+    """This Function will convert an id sequence to a mac address"""
+    return ":".join(map(lambda i: "%02x" % ord(i), octects))
 
-def munge_ipaddress(octects):
+
+def munge_ipaddress_ids(octects):
     return ".".join(octects)
 
-def munge_ipaddress_raw(octects):
+def munge_ipaddress(octects):
     return ".".join(map(lambda x: str(ord(x)),octects))
